9fd6946b3de0bbae29866f79bcefc30c3b7825ef
Trem de Programador <rosjdr@gmail.com>
2022-07-08 14:47:57 -0300

    Implementação da função imprime_cartela

--- bingo.c
+++ bingo.c
@@ -3,32 +3,35 @@
 #include <time.h>
 
 #define TAMANHO_CARTELA 10
+#define QTDE_BOLAS 90
+
+void imprime_cartela(int c[TAMANHO_CARTELA]);
 
 /*
 Implementação de um jogo de bingo em C
 */
 int main(){
     int cartela[TAMANHO_CARTELA];
-    int bolas[90] = {0};
+    int bolas[QTDE_BOLAS] = {0};
 
     srand((unsigned)time(NULL));
 
     //coloca as bolas no saco
-    for(int i=0;i<90;i++){
+    for(int i=0;i<QTDE_BOLAS;i++){
         bolas[i] = i+1;
     }
 
     //gera uma cartela de bingo
     for(int i=0;i<TAMANHO_CARTELA;i++){
-        cartela[i] = rand()%90 + 1;
+        cartela[i] = rand()%QTDE_BOLAS + 1;
     }
     //imprime a cartela de bingo
+    imprime_cartela(cartela);
+}
+
+void imprime_cartela(int c[TAMANHO_CARTELA]){
     for(int i=0;i<TAMANHO_CARTELA;i++){
-        printf("%d ", cartela[i]);
+        printf("%d ", c[i]);
         if (i == 4) printf("\n");
     }
-
-
-
-
 }
